32deef77dbfe05c007a01ea436d85a8c5c185f23
Jacy Wang <jacywj@gmail.com>
2014-05-21 22:18:10 -0400

    All all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrottenpotatoes::Application.config.secret_key_base = 'bdb2dad88b0e415bd5c39b730e1666666df93077fd6450bc9efa5b96ea75e534b20cf22b349800553768fe85f9d6d4eabf3f0bce292e14a1b14fa481cdf1d7ff'
