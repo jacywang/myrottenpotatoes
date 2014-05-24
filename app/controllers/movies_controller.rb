7305334a7554997e2852588987a41ef7e3b4c0ab
Jacy Wang <jacywj@gmail.com>
2014-05-23 21:26:22 -0400

    Add new and create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,12 @@ class MoviesController < ApplicationController
 		id = params[:id]
 		@movie = Movie.find(id)
 	end
+	def new
+		# default: render 'new' template
+	end
+	def create
+		@movie = Movie.create!(params[:movie])
+		flash[:notice] = "#{@movie.title} was successfully created!"
+		redirect_to movies_path
+	end
 end
