06d783d897d47d0c3c70b63781ecfa7cfcaa4341
Jacy Wang <jacywj@gmail.com>
2014-05-26 20:37:26 -0400

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,20 @@
 class MoviesController < ApplicationController
 	def index
+		@all_ratings = Movie.pluck(:rating).uniq
+		if params[:ratings].nil?
+			@checked = @all_ratings
+			params[:sort].nil? ? @movies=Movie.all : @movies=Movie.find(:all, :order=>params[:sort]) 
+		else
+			@checked = params[:ratings].keys
+			@movies=Movie.where(:rating => params[:ratings].keys)
+		end
+
 		if params[:sort] == 'title'
 			@title_hilite = 'hilite'
 		elsif params[:sort] == 'release_date'
 			@release_date_hilite ='hilite'
 		end
-		@movies = Movie.find(:all, :order => params[:sort])
+		
 	end
 	def show
 		id = params[:id]
